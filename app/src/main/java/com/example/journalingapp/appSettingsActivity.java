cdd6b7f75dbeae4953034c7f54b8e2277063d5af
davisProjectss <enhw54110@gmail.com>
2020-11-17 14:38:33 -0500

    Finished App Settings Layout Added a color picker implementation Started to code the user settings

--- app/src/main/java/com/example/journalingapp/appSettingsActivity.java
+++ app/src/main/java/com/example/journalingapp/appSettingsActivity.java
@@ -3,12 +3,64 @@ package com.example.journalingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class appSettingsActivity extends AppCompatActivity {
 
+    int mDefaultColor = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_settings);
+    }
+
+    public void colorTwoTwo(View view) {
+
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, 0, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mDefaultColor = color;
+
+            }
+        });
+    }
+
+    public void colorOneTwo(View view) {
+
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, 0, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mDefaultColor = color;
+
+            }
+        });
+    }
+
+    public void colorOneOne(View view) {
+
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, 0, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mDefaultColor = color;
+
+            }
+        });
     }
 }
