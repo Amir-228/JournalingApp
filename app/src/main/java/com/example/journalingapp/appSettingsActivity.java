11705f3a0c4ce0ad9de7186d882d6b41e8dbc002
davisProjectss <enhw54110@gmail.com>
2020-11-17 15:30:20 -0500

    Added a gradiant drawable for one of the buttons

--- app/src/main/java/com/example/journalingapp/appSettingsActivity.java
+++ app/src/main/java/com/example/journalingapp/appSettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.journalingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,10 +12,14 @@ import yuku.ambilwarna.AmbilWarnaDialog;
 public class appSettingsActivity extends AppCompatActivity {
 
     int mDefaultColor = 0;
+    ConstraintLayout mLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_settings);
+
+        mLayout = findViewById(R.id.appSettingsLayout);
     }
 
     public void colorTwoTwo(View view) {
@@ -30,6 +36,8 @@ public class appSettingsActivity extends AppCompatActivity {
 
             }
         });
+
+        colorPicker.show();
     }
 
     public void colorOneTwo(View view) {
@@ -42,10 +50,18 @@ public class appSettingsActivity extends AppCompatActivity {
 
             @Override
             public void onOk(AmbilWarnaDialog dialog, int color) {
-                mDefaultColor = color;
+                GradientDrawable gd = new GradientDrawable();
+
+                gd.setOrientation(GradientDrawable.Orientation.BL_TR);
+
+                gd.setColors(new int[]  {color, 0});
+
+                mLayout.setBackground(gd);
 
             }
         });
+
+        colorPicker.show();
     }
 
     public void colorOneOne(View view) {
@@ -62,5 +78,7 @@ public class appSettingsActivity extends AppCompatActivity {
 
             }
         });
+
+        colorPicker.show();
     }
 }
