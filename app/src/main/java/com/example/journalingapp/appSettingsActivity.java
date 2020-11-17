14b78dbcbe2688b242af67bd38fc592af8687c86
davisProjectss <enhw54110@gmail.com>
2020-11-17 15:43:55 -0500

    Merged code from last two commits

--- app/src/main/java/com/example/journalingapp/appSettingsActivity.java
+++ app/src/main/java/com/example/journalingapp/appSettingsActivity.java
@@ -1,8 +1,13 @@
 package com.example.journalingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
@@ -10,11 +15,34 @@ import yuku.ambilwarna.AmbilWarnaDialog;
 public class appSettingsActivity extends AppCompatActivity {
 
     int mDefaultColor = 0;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_settings);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.settingsmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.Home:
+                goHome();
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
 
     public void colorTwoTwo(View view) {
 
@@ -58,9 +86,13 @@ public class appSettingsActivity extends AppCompatActivity {
 
             @Override
             public void onOk(AmbilWarnaDialog dialog, int color) {
-                mDefaultColor = color;
 
             }
         });
+    }
+
+    public void goHome(){
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
     }
 }
